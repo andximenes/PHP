154e29867de3cf53b702f74d8bddf9c6bdd4cff2
André Ximenes <andre1ximenes92@gmail.com>
2024-07-17 15:35:31 -0300

    Aula - 20 Validação de Formulário

---
+++ aula20/obrigado.php
@@ -0,0 +1,3 @@
+<?php 
+    echo "<h1>Formulário enviado!</h1>"
+?>
--- aula20/validFormulario.php
+++ aula20/validFormulario.php
@@ -1,14 +1,55 @@
 <?php
-    
-    if($_SERVER['REQUEST_METHOD'] == 'post'){ //Verifica se o método de envio é o método POST
-        if(empyt($_POST['nome'])){ //verifica se o campo está vazio
+    $erroNome = "";
+    $erroEmail = "";
+    $erroSenha = "";
+    $erroRepeteSenha = "";
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){ //Verifica se o método de envio é o método POST
+        if(empty($_POST['nome'])){ //verifica se o campo está vazio
             $erroNome = "Por favor, preencha um nome";
         }else{
             $nome = evitaInjecaoCodigo($_POST['nome']);
             //verifica se os caracteres da variável $nome são strings ou não e retornar true or false, ou seja, se preg_match for diferente dos caracteres especificados, o mesmo irá retornar true e imprimirá na tela a mensagem de erro da variável $erroNome.
-            if(!preg_match("[/a-ZA-Z'/]", $nome)){
-                $erroNome = "Utilize apenas Letras";
+            //Não permite acentos !preg_match("/^[a-zA-Z-' ]*$/", $nome){}
+            //Permite Nomes com acentos acentos
+            if(!preg_match("/^[a-zA-Zá-úÁ-Úà-ùÀ-Ùã-õÃ-Õâ-ûÂ-Ûé-êÉ-Êí-îÍ-Îó-ôÓ-Ôú-ûÚ-ÛçÇ' ]*$/", $nome)){
+                $erroNome = "Utilize apenas letras e acentos";
             }
+            
+        }
+
+        //CAMPO E-MAIL
+        if(empty($_POST['email'])){
+            $erroEmail = "Informe um e-mail";
+        }else{
+            $email = evitaInjecaoCodigo($_POST['email']);
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $erroEmail = "E-mail inválido";
+            }
+        }
+
+        //CAMPO SENHA
+        if(empty($_POST['senha'])){
+            $erroSenha = "Infome uma senha";
+        }else{
+            $senha = evitaInjecaoCodigo($_POST['senha']);
+            if(strlen($senha) < 6){
+                $erroSenha = "A senha deve ter no mínimo 6 dígitos";
+            }
+        }
+        // CAMPO REPETE SENHA
+        if(empty($_POST['repeteSenha'])){
+            $erroRepeteSenha = "repita a senha";
+        }else{
+            $repeteSenha = evitaInjecaoCodigo($_POST['repeteSenha']);
+            if($repeteSenha !== $senha){
+                $erroRepeteSenha= "As senhas não conferem";
+            }
+        }
+
+        //Se não houver erro envia o form para a página de cadastro realizado
+        if(($erroNome == "") && ($erroEmail == "") && ($erroSenha == "") && ($erroRepeteSenha == "")){
+            header('Location: obrigado.php');
         }
     }
 
@@ -34,23 +75,24 @@
             <h1>Validação de Formulário</h1>
             <fieldset>
                 <label for="nome">Nome Completo</label>
-                <input type="text" name="nome" placeholder="Digite seu nome" required>
-                <span class="erro"></span>
+                <input type="text" 
+                <?php if(!empty($erroNome)){ echo "class='invalido'";}?> <?php if(isset($_POST['nome'])){echo "value= '".$_POST['nome']."'";} ?> name="nome" placeholder="Digite seu nome" >
+                <span class="erro"><?php echo $erroNome; ?></span>
             </fieldset>
             <fieldset>
                 <label for="email">E-mail</label>
-                <input type="email" name="email" placeholder="exemplo@provedor.com.br" required>
-                <span class="erro"></span>
+                <input type="email" <?php if(!empty($erroEmail)){ echo "class='invalido'";}?> <?php if(isset($_POST['nome'])){echo "value= '".$_POST['email']."'";} ?> name="email" placeholder="exemplo@provedor.com.br" >
+                <span class="erro"><?php echo $erroEmail; ?></span>
             </fieldset>
             <fieldset>
-                <label for="password">Senha</label>
-                <input type="password" name="password" required>
-                <span class="erro"></span>
+                <label for="senha">Senha</label>
+                <input type="password" <?php if(!empty($erroSenha)){ echo "class='invalido'";}?><?php if(isset($_POST['nome'])){echo "value= '".$_POST['senha']."'";} ?> name="senha" >
+                <span class="erro"><?php echo $erroSenha; ?></span>
             </fieldset>
             <fieldset>
-                <label for="password">Confirma senha</label>
-                <input type="password" name="password" required>
-                <span class="erro"></span>
+                <label for="repeteSenha">Confirma senha</label>
+                <input type="password" <?php if(!empty($erroRepeteSenha)){ echo "class='invalido'";}?> <?php if(isset($_POST['nome'])){echo "value= '".$_POST['repeteSenha']."'";} ?> name="repeteSenha" >
+                <span class="erro"><?php echo $erroRepeteSenha; ?></span>
             </fieldset>
     
             <button type="submit" name="submit" value="Enviar Formulário">Enviar</button>
